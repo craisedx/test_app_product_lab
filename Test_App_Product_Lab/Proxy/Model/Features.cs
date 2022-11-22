16453dd5b9e35dc158c8a5a57061dde5b3ec2dbf
Дмитрий Медведев <greatsmith184@gmail.com>
2022-11-22 23:44:29 +0300

    Added some proxy model.

---
+++ Test_App_Product_Lab/Proxy/Model/Features.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_App_Product_Lab.Proxy.Model
+{
+    public class Features
+    {
+        [JsonProperty("static")]
+        public bool IsStatic { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("supported_protocols")]
+        public Dictionary<string,bool> SupportedProtocols { get; set; }
+    }
+}
